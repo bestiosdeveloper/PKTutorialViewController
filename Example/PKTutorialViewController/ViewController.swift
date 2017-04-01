50a2f4ad280a116e47b6c48663c9f757dd20e9a4
Pramod Kumar <pramod.kumar@appinventiv.com>
2017-04-01 23:21:43 +0530

    Initial Commit

---
+++ Example/PKTutorialViewController/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PKTutorialViewController
+//
+//  Created by pramod.kumar@appinventiv.com on 04/01/2017.
+//  Copyright (c) 2017 pramod.kumar@appinventiv.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/PKTutorialViewController/PKTutorialViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PKTutorialViewController : NSObject
+@end
+@implementation PodsDummy_PKTutorialViewController
+@end
